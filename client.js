56decae97ad033e652b8aaee41173e9b4f30d481
ss-aa123 <neermas1@hotmail.com>
2023-02-18 18:43:03 -0500

    refactor code: seperate connect function into its own client module

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require('net');
+const PORT = 50541;
+const IP = 'localhost'
+
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('data', function(data){
+    console.log(data);
+  });
+
+  return conn;
+
+};
+
+module.exports = {
+  connect
+};
