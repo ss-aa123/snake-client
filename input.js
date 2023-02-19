91a4c87586b2513ae3630c828f233bc908356d38
ss-aa123 <neermas1@hotmail.com>
2023-02-19 00:47:38 -0500

    connect user input keys and associated movements to server

--- input.js
+++ input.js
@@ -1,21 +1,44 @@
+const { connect } = require("./client");
 
+
+let connection;
+
+const moves = {
+  'w' : 'Move: up',
+  'a' : 'Move: left',
+  's' : 'Move: down',
+  'd' : 'Move: right',
+};
 
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
-  stdin.on('data', handleUserInput);
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
 
   return stdin;
 };
 
+
 const handleUserInput = function(key) {
+  const stdout = process.stdout;
+
   if (key === '\u0003') {
     process.exit();
   }
+
+  if (moves[key]) {
+    connection.write(moves[key]);
+  }
+
+
 
   
 };
